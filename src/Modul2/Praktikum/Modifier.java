7823fa82798623d2760a2f9caf7c7a1873962a91
Zainul-Code <zainulmuhajir44@gmail.com>
2021-10-19 12:08:34 +0700

    Praktikum tentang Modifier

---
+++ src/Modul2/Praktikum/Modifier.java
@@ -0,0 +1,11 @@
+package Modul2.Praktikum;
+
+public class Modifier {
+    int angkaNon =1;
+    public int angkaPublic=10;
+    protected int angkaProtected =1000;
+    private int angkaPrivate =100;
+
+
+}
+
---
+++ src/Modul2/Tugas/Main.java
@@ -0,0 +1,4 @@
+package Modul2.Tugas;
+
+public class Main {
+}
