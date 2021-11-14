9a86dc33c098da10882b9a90200d7f36c0ef9b5d
Zainul-Code <zainulmuhajir44@gmail.com>
2021-11-14 15:06:38 +0700

    Tugas

--- src/Modul5/Tugas/Perbankan/Tabungan.java
+++ src/Modul5/Tugas/Perbankan/Tabungan.java
@@ -1,4 +1,32 @@
 package Modul5.Tugas.Perbankan;
 
 public class Tabungan {
+    private int saldo;
+
+    public Tabungan(int inisaldo) {
+        this.saldo = inisaldo;
+    }
+
+    public void simpanUang(int jumlah) {
+        //Substraction assignment
+        //saldo = saldo + jumlah;
+        saldo += jumlah;
+    }
+
+    public boolean ambilUang(int jumlah) {
+        //Substraction assignment
+        //saldo = saldo - jumlah;
+        if (jumlah < saldo) {
+            saldo -= jumlah;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getSaldo () {
+        return saldo;
+    }
 }
+
+
